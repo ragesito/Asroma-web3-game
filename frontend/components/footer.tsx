2b0f691ee2e64fed49904f41c55302dbe07fd179
ragesito <ragesito@outlook.com>
2026-01-12 04:25:13 +0100

    feat(footer): add legal links

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,16 +1,34 @@
-import React from "react";
+"use client";
 
+import React from "react";
+import { useRouter } from "next/navigation";
 const Footer = () => {
+  const router = useRouter();
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="text-white/70 mt-1 ">
-      <div className="max-w-6xl mx-auto text-center">
-        <p className="text-sm">
-          &copy; {currentYear} Asroma. All Rights Reserved.
-        </p>
-      </div>
-    </footer>
+    <footer className="relative md:fixed md:bottom-1 bottom-24 w-full py-0 bg-black/40 backdrop-blur-sm">
+  <div className="max-w-6xl mx-auto px-4 flex flex-wrap justify-center items-center gap-4 text-sm text-white/60 ">
+    <span>© {currentYear} <span className="text-white/80 font-medium">Asroma.</span> All Rights Reserved.</span>
+    <span className="hidden md:inline text-white">•</span>
+    <div className="flex space-x-4">
+    <button
+      onClick={() => router.push("/legal?tab=terms")}
+      className="hover:text-white transition"
+    >
+      Terms of Service
+    </button>
+
+    <button
+      onClick={() => router.push("/legal?tab=privacy")}
+      className="hover:text-white transition"
+    >
+      Privacy Policy
+    </button>
+    </div>
+  </div>
+</footer>
+
   );
 };
 
